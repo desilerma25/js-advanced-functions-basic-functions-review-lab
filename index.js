bdb4800b8521ece5644b6eae45147de37316267e
Desiree Lerma <desi.lerma96@yahoo.com>
2021-02-09 13:18:11 -0600

    commit tree bb1212021a646cad5267e6becb4919adcbd428d1

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+} 
+
+let mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(style="*") {
+    return function(adj="special") {
+        return `You are ${style}${adj}${style}!`
+    }
+}
+
+const Calculator = {
+    add: function(n1, n2){
+        return n1 + n2
+    },
+    subtract: function(n1, n2){
+        return n1 - n2
+    },
+    multiply: function(n1, n2){
+        return n1 * n2
+    },
+    divide: function(n1, n2){
+        return n1 / n2
+    } 
+}
+
+function actionApplyer(start, functions) {
+    // returns given start point when the array is empty
+    if (functions.length === 0) {
+        return start
+    }
+    else {
+        // given 13, return 4 after being acted on by sev. funct.
+        let i = 0
+        while (i < functions.length) {
+            start = functions[i](start)
+            i++
+        }
+        return start
+    }
+}
